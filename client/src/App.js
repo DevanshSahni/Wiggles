344f9ff0c2b499b688be248ef9d59c16c711473e
Devansh <devanshsahni@gmail.com>
2023-07-17 21:45:41 +0530

    Create Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import Navbar from "./Components/Navbar";
+import Profile from "./Components/Profile";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      
+      <Navbar/>
+      <Routes>
+        <Route path="/Profile" element={<Profile/>}/>
+      </Routes>
     </div>
   );
 }
