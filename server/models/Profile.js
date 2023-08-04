2fce2eddeff25fd5f50978be63e2e6802c8d0ab3
anu0108 <108670241+anu0108@users.noreply.github.com>
2023-08-04 13:22:04 +0530

    Create Validation through react and jwt merged

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -2,17 +2,20 @@ const mongoose = require ("mongoose");
 
 const ProfileSchema = new mongoose.Schema({
     name:{type:String,required:true},
-    dob:{type:Date,required:true},
+    dob:{type:Date},
     breed:{type:String,required:true},
     gender:{type:String,required:true},
     playdate:{type:Boolean},
-    img:
-    {
-        data: Buffer,
-        contentType: String
+    // image:
+    // {
+    //     data: Buffer,
+    //     contentType: String
+    // },
+    image:{
+        type:String
     },
     userOwner:{type: mongoose.Schema.Types.ObjectId, ref:"UsersCollection" }
-})
+}) 
 
 const ProfileModel = mongoose.model("profile",ProfileSchema)
 
