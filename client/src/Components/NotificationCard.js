34dddf9b698162c4dabb81764e1570f364e1d20d
reach-anu <reachanuuu@gmail.com>
2023-08-14 22:11:59 +0530

    create all notification

--- client/src/Components/NotificationCard.js
+++ client/src/Components/NotificationCard.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfilePhoto from "../images/profilephoto.png"
-import {RiUserAddFill} from "react-icons/ri";
-const NotificationCard = () => {
+import {PiDogFill} from "react-icons/pi";
+import {AiOutlineClose,AiOutlineCheck} from "react-icons/ai"
+
+const NotificationCard = ({allnotificationactive}) => {
+  const [iconClicked,setIconClicked]=useState(false);
+  function HandleIconClick(){
+    setIconClicked(true);
+    seNotificationText("Playdate accepted")
+  }
+  const [notificationText,seNotificationText]=useState("User is requesting for a playdate");
   return (
-    <div className='cardContainer'>
-      <img src={ProfilePhoto} alt="profile-img"></img>
+    <div className={`cardContainer ${(allnotificationactive ? "allNotificationCardContainer":"dropDownCardContainer")}`}>
+      <img className={ allnotificationactive ? "allNotificationImg" : "cardImage" } src={ProfilePhoto} alt="profile-img"></img>
       <div className='dogInfoContainer'>
         <div className='dogInformation'>
-          <h2>Dog Name</h2>
-          <p>User is requesting for a playdate</p>
+          <h2 className={ allnotificationactive ? "allNotificationName" : "cardDogName" }>Dog Name</h2>
+          <p className={ allnotificationactive ? "allNotificationText" : "cardText" }>{notificationText}</p>
         </div>
         <div className='addPlaydate'>
-            <RiUserAddFill className='addPlaydateIcon'/>
+            <AiOutlineCheck className={`addPlaydateIcon ${(iconClicked ? "inactive" : null)}`} onClick={HandleIconClick}/>
+            <AiOutlineClose className={`addPlaydateIcon ${(iconClicked ? "inactive" : null)}`}/>
+            {iconClicked ? <AiOutlineCheck onClick={HandleIconClick} className='addPlaydateIcon disabled' /> : null}
         </div>
       </div>
     </div>
