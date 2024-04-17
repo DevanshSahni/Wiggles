2e46045ae113a4156c42b0d886e12cd389fb642f
anu0108 <anuragwadhwa786@gmail.com>
2024-04-17 22:51:55 +0530

    fix(Vaccination.js,vaccination.css) : Refactored Code

--- client/src/pages/Vaccination.js
+++ client/src/pages/Vaccination.js
@@ -73,7 +73,6 @@ const Vaccination = () => {
       setVetNumber(data.foundUser.vetNumber);
       setVetAddress(data.foundUser.vetAddress);
       setVaccinations(data.foundUser.vaccinations);
-      console.log(data.foundUser.vaccinations);
     };
     handleContent();
   }, [addVaccination]);
@@ -166,267 +165,266 @@ const Vaccination = () => {
   };
 
   return (
-    <>
-      <div className="vaccinationWrapper">
-        <div
-          className="shareIconContainer"
-          onClick={() => (show ? setShow(0) : setShow(1))}
-          style={{ opacity: print ? 0 : 1 }}
+    <div className="vaccinationWrapper">
+      <div
+        className={`shareIconContainer ${
+          print ? "vaccPrintHide" : "vaccPrintShow"
+        }`}
+        onClick={() => (show ? setShow(0) : setShow(1))}
+      >
+        <BsShareFill />
+      </div>
+      <ShareVaccination
+        show={show}
+        print={print}
+        setPrint={setPrint}
+        userID={userID}
+      />
+      <div className="headerContainer">
+        <div className="logoInfoContainer">
+          <img src={Logo} alt="Website logo" loading="lazy"></img>
+          <h3>Wiggles</h3>
+        </div>
+        <h1>PET HEALTH RECORD</h1>
+      </div>
+      <div className="healthInfoWrapper">
+        <button
+          id="vaccinationButton"
+          className={`editButton ${print ? "vaccPrintHide" : "vaccPrintShow"}`}
+          onClick={handleEdit}
         >
-          <BsShareFill />
-        </div>
-        <ShareVaccination
-          show={show}
-          print={print}
-          setPrint={setPrint}
-          userID={userID}
-        />
-        <div className="headerContainer">
-          <div className="logoInfoContainer">
-            <img src={Logo} alt="Website logo" loading="lazy"></img>
-            <h3>Wiggles</h3>
-          </div>
-          <h1>PET HEALTH RECORD</h1>
-        </div>
-        <div className="healthInfoWrapper">
-          <button
-            id="vaccinationButton"
-            className="editButton"
-            onClick={handleEdit}
-            style={{ opacity: print ? 0 : 1 }}
-          >
-            {" "}
-            {editIcon ? (
-              <AiOutlineEdit className="editIcon" />
-            ) : (
-              <AiOutlineSave className="editIcon" />
-            )}
-            &nbsp;{editbtn}
-          </button>
-          {loading && <VaccinationCardSkeleton />}
-          {!loading && (
-            <>
-              <div className="HealthInfoContainer">
-                <h1 id="vaccination-subheading">Pet's Details</h1>
+          {" "}
+          {editIcon ? (
+            <AiOutlineEdit className="editIcon" />
+          ) : (
+            <AiOutlineSave className="editIcon" />
+          )}
+          &nbsp;{editbtn}
+        </button>
+        {loading && <VaccinationCardSkeleton />}
+        {!loading && (
+          <>
+            <div className="HealthInfoContainer">
+              <h1 id="vaccination-subheading">Pet's Details</h1>
+              <h1>
+                Name: <span>{petName}</span>
+              </h1>
+              <div className="dogHealthInfo">
                 <h1>
-                  Name: <span>{petName}</span>
+                  Breed: <span>{breed}</span>
                 </h1>
-                <div className="dogHealthInfo">
+                <div className="dogWeight">
                   <h1>
-                    Breed: <span>{breed}</span>
-                  </h1>
-                  <div className="dogWeight">
-                    <h1>
-                      Weight:
-                      <input
-                        id="weight"
-                        disabled={inactive}
-                        type="number"
-                        value={weight ?? ""}
-                        onChange={(e) => {
-                          setWeight(e.target.value);
-                        }}
-                        placeholder=""
-                      />
-                    </h1>
-                    <h1 className="dogWeightunit">{weight ? "kg" : ""}</h1>
-                  </div>
-                  <div>
-                    <h1>
-                      Allergies:
-                      <input
-                        disabled={inactive}
-                        type="text"
-                        value={allergies ?? ""}
-                        onChange={(e) => {
-                          setAllergies(e.target.value);
-                        }}
-                      />
-                    </h1>
-                  </div>
-                  <div>
-                    <h1>
-                      Conditions:
-                      <input
-                        disabled={inactive}
-                        type="text"
-                        value={conditions ?? ""}
-                        onChange={(e) => {
-                          setConditions(e.target.value);
-                        }}
-                      />
-                    </h1>
-                  </div>
-                </div>
-              </div>
-              <div className="HealthInfoContainer">
-                <h1 id="vaccination-subheading">Veterinarian's Details</h1>
-                <h1 className="vetNameInfo">
-                  Name:
-                  <h1 className="vetHonorific">{vetName ? "Dr." : ""}</h1>
-                  <input
-                    disabled={inactive}
-                    type="text"
-                    value={vetName ?? ""}
-                    onChange={(e) => {
-                      setVetName(e.target.value);
-                    }}
-                    className="vetName"
-                  />
-                </h1>
-                <div className="vetInfo">
-                  <h1>
-                    Contact Number:
+                    Weight:
                     <input
+                      id="weight"
                       disabled={inactive}
                       type="number"
-                      value={vetNumber ?? ""}
-                      maxLength={10}
+                      value={weight ?? ""}
                       onChange={(e) => {
-                        setVetNumber(e.target.value);
+                        setWeight(e.target.value);
                       }}
+                      placeholder=""
                     />
                   </h1>
+                  <h1 className="dogWeightunit">{weight ? "kg" : ""}</h1>
+                </div>
+                <div>
                   <h1>
-                    Location:
+                    Allergies:
                     <input
                       disabled={inactive}
                       type="text"
-                      value={vetAddress ?? ""}
+                      style={{ width: `${(allergies.length + 1) * 7}px` }}
+                      value={allergies ?? ""}
                       onChange={(e) => {
-                        setVetAddress(e.target.value);
+                        setAllergies(e.target.value);
+                      }}
+                    />
+                  </h1>
+                </div>
+                <div>
+                  <h1>
+                    Conditions:
+                    <input
+                      disabled={inactive}
+                      type="text"
+                      value={conditions ?? ""}
+                      style={{ width: `${(conditions.length + 1) * 7}px` }}
+                      onChange={(e) => {
+                        setConditions(e.target.value);
                       }}
                     />
                   </h1>
                 </div>
               </div>
-            </>
-          )}
-          <div className="vaccinationContainer">
-            <div className="vaccinationInfoPrimary">
-              <h1 id="vaccination-subheading">Vaccinations</h1>
-              <button
-                id="vaccinationButton"
-                form="vaccinationForm"
-                style={{ opacity: print ? 0 : 1 }}
-              >
-                {addVaccination ? (
-                  <AiOutlineSave className="addIcon" />
-                ) : (
-                  <AiOutlinePlus className="addIcon" />
-                )}
-                &nbsp;{addVaccination ? "Save" : "Add"}{" "}
-              </button>
             </div>
-            <form
-              name="Vaccination Form"
-              id="vaccinationForm"
-              onSubmit={handleAddVaccine}
-            ></form>
-            <table className="vaccinationTable">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Batch Number</th>
-                  <th>Date</th>
-                  <th>Next visit</th>
-                  {/* {!editIcon && <th className='deletedVaccination'>Action</th>} */}
-                </tr>
-              </thead>
-              <tbody>
-                {addVaccination && (
-                  <tr className="addVaccinationForm">
-                    <td>
-                      <input
-                        required
-                        type="text"
-                        placeholder="Name"
-                        form="vaccinationForm"
-                        value={visit.name ?? ""}
-                        onChange={(e) =>
-                          setVisit((visit) => ({
-                            ...visit,
-                            name: e.target.value,
-                          }))
-                        }
-                      />
-                    </td>
-                    <td>
-                      <input
-                        required
-                        type="number"
-                        placeholder="Batch no"
-                        className="batch"
-                        form="vaccinationForm"
-                        value={visit.batchNumber ?? ""}
-                        onChange={(e) =>
-                          setVisit((visit) => ({
-                            ...visit,
-                            batchNumber: e.target.value,
-                          }))
-                        }
-                      />
-                    </td>
-                    <td>
-                      <input
-                        required
-                        placeholder="Date"
-                        onFocus={onFocus}
-                        onBlur={onBlur}
-                        form="vaccinationForm"
-                        value={visit.date ?? ""}
-                        onChange={(e) =>
-                          setVisit((visit) => ({
-                            ...visit,
-                            date: e.target.value,
-                          }))
-                        }
-                      />
-                    </td>
-                    <td>
-                      <input
-                        required
-                        placeholder="Next Visit"
-                        onFocus={onFocus}
-                        onBlur={onBlur}
-                        form="vaccinationForm"
-                        value={visit.dueDate ?? ""}
-                        // min={visit.date}
-                        min={visit.date ? getNextDay(visit.date) : ""}
-                        disabled={!visit.date}
-                        onChange={(e) =>
-                          setVisit((visit) => ({
-                            ...visit,
-                            dueDate: e.target.value,
-                          }))
-                        }
-                      />
-                    </td>
-                  </tr>
-                )}
-                {vaccinations &&
-                  vaccinations.map((vaccination) => (
-                    <tr key={vaccination?._id}>
-                      <td>{vaccination?.name}</td>
-                      <td>{vaccination?.batchNumber}</td>
-                      <td>{vaccination?.date.slice(0, 10)}</td>
-                      <td>{vaccination?.dueDate.slice(0, 10)}</td>
-                      {/* {!editIcon && <td><AiFillDelete className='addIcon'/></td>} */}
-                    </tr>
-                  ))}
-                {loading && (
-                  <>
-                    <VaccinationTableSkeleton />
-                    <VaccinationTableSkeleton />
-                    <VaccinationTableSkeleton />
-                  </>
-                )}
-              </tbody>
-            </table>
+            <div className="HealthInfoContainer">
+              <h1 id="vaccination-subheading">Veterinarian's Details</h1>
+              <h1 className="vetNameInfo">
+                Name:
+                <h1 className="vetHonorific">{vetName ? "Dr." : ""}</h1>
+                <input
+                  disabled={inactive}
+                  type="text"
+                  value={vetName ?? ""}
+                  onChange={(e) => {
+                    setVetName(e.target.value);
+                  }}
+                  className="vetName"
+                />
+              </h1>
+              <div className="vetInfo">
+                <h1>
+                  Contact Number:
+                  <input
+                    disabled={inactive}
+                    type="number"
+                    value={vetNumber ?? ""}
+                    maxLength={10}
+                    onChange={(e) => {
+                      setVetNumber(e.target.value);
+                    }}
+                  />
+                </h1>
+                <h1>
+                  Location:
+                  <input
+                    disabled={inactive}
+                    type="text"
+                    value={vetAddress ?? ""}
+                    onChange={(e) => {
+                      setVetAddress(e.target.value);
+                    }}
+                  />
+                </h1>
+              </div>
+            </div>
+          </>
+        )}
+        <div className="vaccinationContainer">
+          <div className="vaccinationInfoPrimary">
+            <h1 id="vaccination-subheading">Vaccinations</h1>
+            <button
+              id="vaccinationButton"
+              form="vaccinationForm"
+              className={`${print ? "vaccPrintHide" : "vaccPrintShow"}`}
+            >
+              {addVaccination ? (
+                <AiOutlineSave className="addIcon" />
+              ) : (
+                <AiOutlinePlus className="addIcon" />
+              )}
+              &nbsp;{addVaccination ? "Save" : "Add"}{" "}
+            </button>
           </div>
+          <form
+            name="Vaccination Form"
+            id="vaccinationForm"
+            onSubmit={handleAddVaccine}
+          ></form>
+          <table className="vaccinationTable">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Batch Number</th>
+                <th>Date</th>
+                <th>Next visit</th>
+                {/* {!editIcon && <th className='deletedVaccination'>Action</th>} */}
+              </tr>
+            </thead>
+            <tbody>
+              {addVaccination && (
+                <tr className="addVaccinationForm">
+                  <td>
+                    <input
+                      required
+                      type="text"
+                      placeholder="Name"
+                      form="vaccinationForm"
+                      value={visit.name ?? ""}
+                      onChange={(e) =>
+                        setVisit((visit) => ({
+                          ...visit,
+                          name: e.target.value,
+                        }))
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      required
+                      type="number"
+                      placeholder="Batch no"
+                      className="batch"
+                      form="vaccinationForm"
+                      value={visit.batchNumber ?? ""}
+                      onChange={(e) =>
+                        setVisit((visit) => ({
+                          ...visit,
+                          batchNumber: e.target.value,
+                        }))
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      required
+                      placeholder="Date"
+                      onFocus={onFocus}
+                      onBlur={onBlur}
+                      form="vaccinationForm"
+                      value={visit.date ?? ""}
+                      onChange={(e) =>
+                        setVisit((visit) => ({
+                          ...visit,
+                          date: e.target.value,
+                        }))
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      required
+                      placeholder="Next Visit"
+                      onFocus={onFocus}
+                      onBlur={onBlur}
+                      form="vaccinationForm"
+                      value={visit.dueDate ?? ""}
+                      min={visit.date ? getNextDay(visit.date) : ""}
+                      disabled={!visit.date}
+                      onChange={(e) =>
+                        setVisit((visit) => ({
+                          ...visit,
+                          dueDate: e.target.value,
+                        }))
+                      }
+                    />
+                  </td>
+                </tr>
+              )}
+              {vaccinations &&
+                vaccinations.map((vaccination) => (
+                  <tr key={vaccination?._id}>
+                    <td>{vaccination?.name}</td>
+                    <td>{vaccination?.batchNumber}</td>
+                    <td>{vaccination?.date.slice(0, 10)}</td>
+                    <td>{vaccination?.dueDate.slice(0, 10)}</td>
+                    {/* {!editIcon && <td><AiFillDelete className='addIcon'/></td>} */}
+                  </tr>
+                ))}
+              {loading && (
+                <>
+                  <VaccinationTableSkeleton />
+                  <VaccinationTableSkeleton />
+                  <VaccinationTableSkeleton />
+                </>
+              )}
+            </tbody>
+          </table>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
