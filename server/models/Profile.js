8f4730f359b88b1767a8786931b0b5ecb9f9e5ab
anu0108 <anuragwadhwa786@gmail.com>
2024-06-02 16:05:34 +0530

    feat: update Profile Schema

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,47 +1,50 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
-const NotificationSchema= new mongoose.Schema({
-    title : {type: String},
-    message : {type: String},
-    friendID: {type:  mongoose.Schema.ObjectId},
-    date: { type: Date, default: Date.now },
-    viewed: {type: Boolean, default: false},
-    image: {type: String},
-})
+const NotificationSchema = new mongoose.Schema({
+  title: { type: String },
+  message: { type: String },
+  friendID: { type: mongoose.Schema.ObjectId },
+  date: { type: Date, default: Date.now },
+  viewed: { type: Boolean, default: false },
+  image: { type: String },
+});
 
-const VaccinationSchema= new mongoose.Schema({
-    name:{type:String},
-    batchNumber:{type: Number},
-    date:{type: Date},
-    dueDate:{type:Date},
-})
+const VaccinationSchema = new mongoose.Schema({
+  name: { type: String },
+  batchNumber: { type: Number },
+  date: { type: Date },
+  dueDate: { type: Date },
+});
 
 const ProfileSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    dob:{type:Date},
-    breed:{type:String,required:true},
-    gender:{type:String,required:true},
-    vaccinated:{type:Boolean},
-    image:{
-        type:String
-    },
-    bio:{type: String},
-    dueDate:{type: Date},
-    address: {type: String},
-    friends: [{type: mongoose.Schema.ObjectId}],
-    requestSent: [{type: mongoose.Schema.ObjectId}],
-    requestRecieved: [{type: mongoose.Schema.ObjectId}],
-    notifications: [{type: NotificationSchema}],
-    height: {type: Number},
-    weight: {type: Number},
-    allergies: {type: String},
-    conditions: {type: String},
-    vetName: {type: String},
-    vetNumber: {type: Number},
-    vetAddress: {type: String},
-    vaccinations: [{type: VaccinationSchema}]
-}) 
+  name: { type: String, required: true },
+  dob: { type: Date },
+  breed: { type: String, required: true },
+  gender: { type: String, required: true },
+  vaccinated: { type: Boolean },
+  image: {
+    type: String,
+  },
+  bio: { type: String },
+  dueDate: { type: Date },
+  address: { type: String },
+  friends: [{ type: mongoose.Schema.ObjectId }],
+  requestSent: [{ type: mongoose.Schema.ObjectId }],
+  requestRecieved: [{ type: mongoose.Schema.ObjectId }],
+  notifications: [{ type: NotificationSchema }],
+  height: { type: Number },
+  weight: { type: Number },
+  allergies: { type: String },
+  conditions: { type: String },
+  vetName: { type: String },
+  vetNumber: { type: Number },
+  vetAddress: { type: String },
+  vaccinations: [{ type: VaccinationSchema }],
+  warn: { type: Boolean, default: false },
+  warnings: { type: Number, default: 0 },
+  ban: { type: Boolean, default: false },
+});
 
-const ProfileModel = mongoose.model("profile",ProfileSchema)
+const ProfileModel = mongoose.model("profile", ProfileSchema);
 
-module.exports = ProfileModel;   
+module.exports = ProfileModel;
