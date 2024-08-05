df460a852f72f20c18a84a35a903ff5e3ec243c3
anuragtj <anuragwadhwa786@gmail.com>
2024-08-05 19:46:41 +0530

    fix: remove violation redux code

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -36,7 +36,6 @@ const LandingPage = () => {
       const response = await postData("login", { email, password });
       if (response.data.status === "ok") {
         dispatch(loginUser(true));
-        // await dispatch(fetchViolations());
         navigate("/profile");
       } else {
         toast.warn(response.data.message);
